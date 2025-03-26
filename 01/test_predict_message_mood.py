a80c145183dff69cd62ac74b17679897ae1ced66
Olga <159625075+ovwvwo@users.noreply.github.com>
2025-03-26 16:15:53 +0300

    Update test_predict_message_mood.py

--- 01/test_predict_message_mood.py
+++ 01/test_predict_message_mood.py
@@ -1,1 +1,15 @@
 # TODO: Implement
+# 01/test_predict_message_mood.py
+import unittest
+from predict_message_mood import predict_message_mood
+
+class TestPredictMessageMood(unittest.TestCase):
+    
+    def test_predict_message_mood(self):
+        # Тесты для различных порогов
+        self.assertEqual(predict_message_mood("Чапаев и пустота"), "отл")
+        self.assertEqual(predict_message_mood("Чапаев и пустота", 0.8, 0.99), "норм")
+        self.assertEqual(predict_message_mood("Вулкан"), "неуд")
+
+if __name__ == "__main__":
+    unittest.main()
